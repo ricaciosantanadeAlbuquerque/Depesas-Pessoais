bdd7e85681b8c85c5e750d3e46cf11cdbecb51ee
Ricacio Santana de Albuquerque <ricaciozz@gmail.com>
2023-11-28 01:13:22 -0300

    lista de transações

--- depesas_pessoais/lib/view/home_page.dart
+++ depesas_pessoais/lib/view/home_page.dart
@@ -1,3 +1,6 @@
+import 'dart:math';
+import 'package:intl/intl.dart';
+import 'package:depesas_pessoais/model/transaction.dart';
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatefulWidget {
@@ -7,24 +10,65 @@ class MyHomePage extends StatefulWidget {
 }
 
 class MyHomePageState extends State<MyHomePage> {
+  final List<Transaction> listTransaction = [
+    Transaction(id: Random().nextDouble().toString(), title: 'Conta de Luz', value: 150000, date: DateTime.now()),
+    Transaction(id: Random().nextDouble().toString(), title: 'Cartao de crédito', value: 250, date: DateTime.now())
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Despesas Pessoais'),
-        ),
-        body: const Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            Card(elevation: 5, color: Colors.blue, child: Text('Grafico')),
-            Card(elevation: 5, color: Colors.blue, child: Text('Lista')),
-            Card(
-              elevation: 5,
-              color: Colors.blue,
-              child: Text('Form'),
+      appBar: AppBar(
+        title: const Text('Despesas Pessoais'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          const Card(elevation: 5, color: Colors.blue, child: Text('Grafico')),
+          SizedBox(
+            height: 380,
+            child: ListView.builder(
+              itemCount: listTransaction.length,
+              itemBuilder: (_, index) {
+                final trs = listTransaction[index];
+                return Card(
+                    margin:const EdgeInsets.symmetric(
+                      vertical: 8,
+                      horizontal:5
+                    ),
+                    elevation: 5,
+                    child: ListTile(
+                      leading: CircleAvatar(
+                        radius: 30,
+                        child: Padding(
+                          padding: const EdgeInsets.all(9.0),
+                          child: FittedBox(
+                            child: Text(
+                              trs.value.toStringAsFixed(2),
+                            ),
+                          ),
+                        ),
+                      ),
+                      title: Text(trs.title),
+                      subtitle: Text(DateFormat('dd MMM y').format(trs.date)),
+                      trailing:IconButton(
+                        onPressed:(){},
+                        color:Theme.of(context).colorScheme.error,
+                        icon: const Icon(Icons.delete),
+                        ),
+                    ),
+                    );
+              },
             ),
-          ],
-        ));
+          ),
+          const Card(
+            elevation: 5,
+            color: Colors.blue,
+            child: Text('Form'),
+          ),
+        ],
+      ),
+    );
   }
 }
